86487f3d279bd46ed32fe95c77f937c00f6b15ab
duybinh98 <43360513+duybinh98@users.noreply.github.com>
2020-04-20 14:57:46 +0700

    Updates

---
+++ precache-manifest.3fcef0565d0f8a9a8a57f6b1729b93e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc88f26e26140e5e7218",
+    "url": "/MedTestForCoordinator/static/js/app.729c7822.chunk.js"
+  },
+  {
+    "revision": "15eee14489800ef29d40",
+    "url": "/MedTestForCoordinator/static/js/runtime~app.90dec588.js"
+  },
+  {
+    "revision": "01f2caa10bc211d940ba",
+    "url": "/MedTestForCoordinator/static/js/2.610fae2d.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/MedTestForCoordinator/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/MedTestForCoordinator/expo-service-worker.js"
+  },
+  {
+    "revision": "e10ce0552ee7802193950fa9181bd961",
+    "url": "/MedTestForCoordinator/favicon-16.png"
+  },
+  {
+    "revision": "b333dcaccf681a69a1b0a5058aa06fcb",
+    "url": "/MedTestForCoordinator/favicon-32.png"
+  },
+  {
+    "revision": "62273473911e68575ace8d48f6ebd399",
+    "url": "/MedTestForCoordinator/favicon.ico"
+  },
+  {
+    "revision": "1a0929438da3730b30b2612a756311e2",
+    "url": "/MedTestForCoordinator/index.html"
+  },
+  {
+    "revision": "482a2d492f761b360025ca5de46ce1b8",
+    "url": "/MedTestForCoordinator/manifest.json"
+  },
+  {
+    "revision": "f833a04df7dbcc669aac90428d389f8d",
+    "url": "/MedTestForCoordinator/register-service-worker.js"
+  }
+];
